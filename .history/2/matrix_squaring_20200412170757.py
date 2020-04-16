5bb4f7be1a0f51afda6ffb0d08c275d4283ec0ac
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-04-15 19:04:37 -0500

    .gitignore fix

--- .history/2/matrix_squaring_20200412170757.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division
-import numpy as np
-import matplotlib.pyplot as plt
-from time import time
-
-def rho_free(x,xp,beta):
-    return (2.*np.pi*beta)**(-0.5) * np.exp(-(x-xp)**2 / (2 * beta) )
--- .history/2/path_integral_naive_sampling_20200415115916.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division
-import numpy as np
-import matplotlib.pyplot as plt
-from time import time
-import pandas as pd
-
-
-# Author: Juan Esteban Aristizabal-Zuluaga
