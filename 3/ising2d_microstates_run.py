bf2cce8107e8aaa2c49a2560e4daeeca691e5884
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-05-09 19:06:11 -0500

    Substancial progress in project 3, first part almost done.

--- 3/ising2d_microstates_run.py
+++ 3/ising2d_microstates_run.py
@@ -5,14 +5,18 @@ from ising2d_microstates import *
 # PANEL DE CONTROL
 ################################################################################################
 
-# Decide si corre algoritmo matrix squaring con aproximación de trotter
+# Decide si corre algoritmo para calcular microestados de energía
 run_microstates_algorithm = False
 
-# Decide si corre algoritmo para cálculo de energía interna
-run_Z_contributions = True
+# Decide si corre algoritmo para cálculo de contribuciones a la función partición
+# por cada valor de energía 
+run_Z_contributions_algorithm = False
+
+# Decide si corre algoritmo de aproximación de función partición
+run_Z_approx_algorithm = True
 
 # Decide si corre algoritmo para optimización de dx y beta_ini
-run_optimization = False
+run_specific_heat_algorithm = False
 
 
 
@@ -45,19 +49,46 @@ if run_microstates_algorithm:
     print(pd.DataFrame(microstate_rand.reshape((L,L))), '\n')
     ising_microstate_plot(microstate_rand, save_plot=True)
 
-if run_Z_contributions:
+if run_Z_contributions_algorithm:
 
-    kwargs = {'energies': None,
-              'L': 5,
-              'beta': 0.005,
-              'beta_max': None,
-              'N_values': None,
-              'read_data': True,
-              'energy_data_file_name': None,
-              'plot_histogram': True,
-              'show_plot': True,
-              'save_plot': True,
-              'plot_file_Name': None,
-              }
+    kwargs = {
+        'microstate_energies': None,
+        'L': 5,
+        'beta': 0.5,
+        'beta_max': None,
+        'N_beta': 100,
+        'read_data': True,
+        'energy_data_file_name': None,
+        'plot_histogram': True,
+        'show_plot': True,
+        'save_plot': True,
+        'plot_file_Name': None,
+        }
     
-    partition_func(**kwargs)
+    Z_array, statistical_weights_array, beta_array, energies, omegas = \
+        partition_func_stat_weights(**kwargs)
+    
+    # Acá está la clave para  la pregunta de la mitad en la pag 5   
+    # el gráfico de log(Z) es lineal a altos beta ya que Z es aproximadamente una exponencial para estos valores
+    # plt.plot(beta_array, np.log(Z_array))
+    # plt.show()
+
+if run_Z_approx_algorithm:
+    approx_partition_func(read_data=True, save_plot=True)
+
+if run_specific_heat_algorithm:
+    
+    kwargs = {
+        'microstate_energies_array': [None, None, None, None],
+        'L_array': [2, 3, 4, 5],
+        'beta_min': 1/8,
+        'beta_max': 10,
+        'N_beta': 1000,
+        'read_data': True,
+        'energy_data_file_name': None,
+        'show_plot': True,
+        'save_plot': True,
+        'plot_file_Name': None,
+        }
+
+    plot_specific_heat_cv(**kwargs)
